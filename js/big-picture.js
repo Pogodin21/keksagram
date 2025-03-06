f24df3c736bd323558028f9acd1ebef24edf662d
Pogodin21 <132946710+Pogodin21@users.noreply.github.com>
2025-03-07 00:38:30 +0400

    Рефакторинг и стилизация кода в big-picture.js

--- js/big-picture.js
+++ js/big-picture.js
@@ -6,46 +6,43 @@ const body = document.querySelector('body');
 const commentsLoader = document.querySelector('.comments-loader');
 const cancelButton = document.querySelector('.big-picture__cancel');
 
-
 let commentsShown = 0;
 let comments = [];
 
+const createElement = (tag, className, attributes = {}) => {
+    const element = document.createElement(tag);
+    element.classList.add(className);
+    Object.assign(element, attributes);
+    return element;
+};
 
 const addComment = function () {
-    const newComment = document.createElement('li');
-    newComment.classList.add('social__comment');
-    const newPictureComment = document.createElement('img');
-    newPictureComment.classList.add('social__picture');
-    newPictureComment.src = "";
-    newPictureComment.alt = "";
-    newPictureComment.width = "35";
-    newPictureComment.height = "35";
-    const newTextComment = document.createElement('p');
-    newTextComment.classList.add('social__text');
-    newTextComment.textContent = "";
-    newComment.appendChild(newPictureComment);
-    newComment.appendChild(newTextComment);
-    return newComment;
+  const newComment = createElement('li', 'social__comment');
+  const newPictureComment = createElement('img', 'social__picture', {
+      src: "",
+      alt: "",
+      width: 35,
+      height: 35
+  });
+  const newTextComment = createElement('p', 'social__text', { textContent: "" });
 
-  };
+  newComment.append(newPictureComment, newTextComment);
+  return newComment;
+};
 
 const createComment = ({avatar, name, message}) => {
     const comment = addComment();
     comment.querySelector('.social__picture').src = avatar;
     comment.querySelector('.social__picture').alt = name;
     comment.querySelector('.social__text').textContent = message;
-
     return comment;
-}
-
-
-
+};
 
 const renderComments = () => {
   commentsShown += COMMENTS_PER_PORTION;
 
   if (commentsShown >= comments.length) {
-     commentsLoader.classList.add('hidden');
+    commentsLoader.classList.add('hidden');
     commentsShown = comments.length;
   } else {
     commentsLoader.classList.remove('hidden');
@@ -60,24 +57,24 @@ const renderComments = () => {
   commentList.innerHTML = '';
   commentList.appendChild(fragment);
   commentCount.innerHTML = `${commentsShown} из ${comments.length}  комментариев`;
-
-
 };
 
 const hideBigPicture = () => {
     bigPicture.classList.add('hidden');
     body.classList.remove('modal-open');
-    document.removeEventListener('keydown', onDocumentKeydown);
-    document.removeEventListener('click', onCancelButtonClick);
+   
     commentList.innerHTML = '';
     commentsShown = 0;
+
+    document.removeEventListener('keydown', onDocumentKeydown);
+    document.removeEventListener('click', onCancelButtonClick);
 };
 
 function onDocumentKeydown(evt) {
     if (evt.key === 'Escape') {
         evt.preventDefault();
         hideBigPicture();
-    }
+    };
 };
 
 const onCancelButtonClick = () => {
@@ -85,28 +82,29 @@ const onCancelButtonClick = () => {
 };
 
 const renderPictureDetails = ({url, likes, description}) => {
-    bigPicture.querySelector('.big-picture__img img').src = url;
-    bigPicture.querySelector('.big-picture__img img').alt = description;
-    bigPicture.querySelector('.likes-count').textContent = likes;
-    bigPicture.querySelector('.social__caption').textContent = description;
+    const img = bigPicture.querySelector('.big-picture__img img');
+    const likesCount = bigPicture.querySelector('.likes-count');
+    const caption = bigPicture.querySelector('.social__caption');
+
+    img.src = url;
+    img.alt = description;
+    likesCount.testContent = likes;
+    caption.testContent = description;
 };
 
 const showBigPicture = (data) => {
     bigPicture.classList.remove('hidden');
     body.classList.add('modal-open');
-    // commentsLoader.classList.add('hidden');
-    document.addEventListener('keydown', onDocumentKeydown);
-    cancelButton.addEventListener('click', onCancelButtonClick);
+
+    renderPictureDetails(data);
 
     comments = data.comments;
     renderComments();
+  
+    document.addEventListener('keydown', onDocumentKeydown);
+    cancelButton.addEventListener('click', onCancelButtonClick);
     commentsLoader.addEventListener('click', renderComments);
-
-
-
-    renderPictureDetails(data)
-
-}
+};
 
 
 export { showBigPicture };
